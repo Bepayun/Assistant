79476044e0389161867eb7f4918b6c475660a6d7
王若芸 <bepa@BepaBikvy-2.local>
2019-07-31 11:56:00 +0800

    commit

--- is_ass/Views/ViewsLogon/LoginViewController.h
+++ is_ass/Views/ViewsLogon/LoginViewController.h
@@ -16,7 +16,7 @@
 @property(nonatomic,assign)UITextField* Account_Numbertextfield; // 账号
 @property(nonatomic,assign)UITextField* PassCodetextField; // 密码
 @property(nonatomic,assign)UIButton* RegisterButton; // 注册账号
-@property(nonatomic,assign)UIButton* LogInbutton; // 登录
+@property(nonatomic,assign)UIButton* LogInbutton; ///< 登录
 @property(nonatomic,assign)UILabel* VersionLabel;
 @property (nonatomic, retain) UIImageView*  backview;
 
